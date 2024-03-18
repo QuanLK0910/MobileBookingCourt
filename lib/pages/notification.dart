ce4050d12d82e3a77819c5d73b3cfc1f9157ed8d
QuanLK0910 <khanhquan09102003@gmail.com>
2024-03-18 07:51:33 +0700

    merge dkmtech

---
+++ lib/pages/notification.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class notifications extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Column(
+     
+    );
+  }
+}
+class NotificationHeader extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return const Text("Notification");
+  }
+}
